af29d5317d29ac95c9b07380812b245a5fd9874e
Oliver Lee <oliverzlee@gmail.com>
2019-11-10 14:46:44 +0100

    Support .gpx files downloaded from Strava

--- fitbit_hr_tcx/activity.py
+++ fitbit_hr_tcx/activity.py
@@ -43,6 +43,23 @@ class Activity:
         self.xml = minidom.parse(tcx_file)
 
         times = self.xml.getElementsByTagName("Time")
+        if not times:
+            # Try again with lower case
+            times = self.xml.getElementsByTagName("time")
+            # Skip the first one since it's in a metadata element
+            assert times[0].parentNode.tagName == "metadata"
+            assert times[1].parentNode.tagName == "trkpt"
+            assert times[-1].parentNode.tagName == "trkpt"
+            times = times[1:]
+            self.extension = True
+        else:
+            self.extension = False
+
+        # Cleanup time format if invalid isoformat
+        for t in times:
+            if t.childNodes[0].data.endswith("Z"):
+                t.childNodes[0].data = t.childNodes[0].data[:-1] + "+00:00"
+
         self.times = times
 
         times = [times[0], times[-1]]
@@ -89,8 +106,21 @@ class Activity:
         """
         xml = self.xml
 
-        e = xml.createElement("Value")
+        if self.extension and heart_rate_type is not None:
+            raise NotImplementedError(
+                "Heart Rate Element is not defined when using 'TrackPointExtension'"
+                f"and 'heart_rate_type' {heart_rate_type}."
+            )
+
+        if self.extension:
+            e = xml.createElement("gpxtpx:hr")
+        else:
+            e = xml.createElement("Value")
+
         e.appendChild(xml.createTextNode(str(heart_rate_bpm)))
+
+        if self.extension:
+            return e
 
         if heart_rate_type is None:
             element_name_prefix = ""
@@ -107,6 +137,24 @@ class Activity:
 
     def merge_heart_rate(self, heart_rate_samples: List[HeartRateSample]):
         """ Merge heart rate data into the existing activity. """
+
+        def insert(time_node, heart_rate_node):
+            """ Insert the heart rate node in the right place. """
+            is_element = lambda x: isinstance(x, minidom.Element)
+
+            if self.extension:
+                for c in filter(is_element, time_node.parentNode.childNodes):
+                    if c.tagName == "extensions":
+                        extension_node = c
+                        break
+                for c in filter(is_element, extension_node.childNodes):
+                    if c.tagName == "gpxtpx:TrackPointExtension":
+                        tpe_node = c
+                        break
+                tpe_node.appendChild(heart_rate_node)
+            else:
+                time_node.parentNode.appendChild(heart_rate_node)
+
         # Methods __eq__ and __lt__ are defined for `HeartRateSample` so that's
         # always on the left side.
         iter_a = iter(heart_rate_samples)
@@ -117,7 +165,7 @@ class Activity:
         while True:
             try:
                 if a == b:
-                    b.parentNode.appendChild(self.create_heart_rate_element(a.bpm))
+                    insert(b, self.create_heart_rate_element(a.bpm))
                     a = next(iter_a)
                     b = next(iter_b)
                 elif a < b:
@@ -128,18 +176,20 @@ class Activity:
                 break
 
         # Update max and average heart rate as well.
-        max_bpm = max(x.bpm for x in heart_rate_samples)
         max_hr = self.xml.getElementsByTagName("MaximumHeartRateBpm")
-        assert len(max_hr) == 1
-        max_hr = max_hr[0]
-        max_hr.parentNode.replaceChild(
-            self.create_heart_rate_element(max_bpm, "Maximum"), max_hr
-        )
+        if max_hr:
+            assert len(max_hr) == 1
+            max_bpm = max(x.bpm for x in heart_rate_samples)
+            max_hr = max_hr[0]
+            max_hr.parentNode.replaceChild(
+                self.create_heart_rate_element(max_bpm, "Maximum"), max_hr
+            )
 
-        avg_bpm = mean(x.bpm for x in heart_rate_samples)
         avg_hr = self.xml.getElementsByTagName("AverageHeartRateBpm")
-        assert len(avg_hr) == 1
-        avg_hr = avg_hr[0]
-        avg_hr.parentNode.replaceChild(
-            self.create_heart_rate_element(avg_bpm, "Average"), avg_hr
-        )
+        if avg_hr:
+            assert len(avg_hr) == 1
+            avg_bpm = mean(x.bpm for x in heart_rate_samples)
+            avg_hr = avg_hr[0]
+            avg_hr.parentNode.replaceChild(
+                self.create_heart_rate_element(avg_bpm, "Average"), avg_hr
+            )
